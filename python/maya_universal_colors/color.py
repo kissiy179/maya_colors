3e02b2336f3ee10aab89469b8f96768ec51eea1e
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-05 02:37:10 +0900

    colorモジュールがQtGuiをうまく呼べずにエラーになっていたのを解消

--- python/maya_universal_colors/color.py
+++ python/maya_universal_colors/color.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import maya.api.OpenMaya as om2
-from qtpy import *
+from qtpy import QtGui
 
 class Color(object):
 
-    def __init__(self, r=0, g=0, b=0):
-        self.r, self.g, self.b = r, g, b
-        self.values = r, g, b
+    def __init__(self, r=0, g=0, b=0, a=255):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+        self.values = r, g, b, a
 
-    def as_QColor(self):
-        return om2.MColor((i / 255.0 for i in self.values))
+    def __repr__(self):
+        return '{module}.{cls}({r}, {g}, {b}, {a})'.format(module=__name__, cls=type(self).__name__, r=self.r, g=self.g, b=self.b, a=self.a)
 
     def as_MColor(self):
+        return om2.MColor([i / 255.0 for i in self.values])
+
+    def as_QColor(self):
         return QtGui.QColor(*self.values)
