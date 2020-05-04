1746140f6f42a19a32cb64d7410b36d17b0f1979
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-05 03:53:21 +0900

    cudカラーをあつめたディクショナリ名をcud_colorsに変更

--- python/maya_colors/colors.py
+++ python/maya_colors/colors.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 import cud_colors.colors as cud_colors_
 import color
 
-colors = OrderedDict()
+cud_colors = OrderedDict()
 
-for key, values in cud_colors_.colors.all_colors.items():
-    colors[key] = color.Color(*values)
+for key, values in cud_colors_.all_colors.items():
+    cud_colors[key] = color.Color(*values)
