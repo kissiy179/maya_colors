9e7c19058e1752712601c1eea5a0e9a7d5cf4166
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-05 03:02:44 +0900

    Colorクラスにnormalized_valuesアトリビュートを追加

--- python/maya_universal_colors/color.py
+++ python/maya_universal_colors/color.py
@@ -10,12 +10,13 @@ class Color(object):
         self.b = b
         self.a = a
         self.values = r, g, b, a
+        self.normalized_values = tuple(i / 255.0 for i in self.values)
 
     def __repr__(self):
         return '{module}.{cls}({r}, {g}, {b}, {a})'.format(module=__name__, cls=type(self).__name__, r=self.r, g=self.g, b=self.b, a=self.a)
 
     def as_MColor(self):
-        return om2.MColor([i / 255.0 for i in self.values])
+        return om2.MColor(self.normalized_values)
 
     def as_QColor(self):
         return QtGui.QColor(*self.values)
